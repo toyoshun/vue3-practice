d02edc16cd1d4ad96da2f0d418b5438460c2018b
toyo_shun <choka2323@gmail.com>
2021-05-04 12:17:21 +0900

    first commit

---
+++ src/views/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="counter">
+    <button @click="decrementA()">-</button>
+    {{ state.count }}
+    <button @click="incrementA()">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+    function incrementA() {
+      state.count++;
+    }
+    function decrementA() {
+      state.count--;
+    }
+
+    return {
+      state,
+      incrementA,
+      decrementA,
+    };
+  },
+});
+</script>
