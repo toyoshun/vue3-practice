c8303a00c0a68783d379ade5483c2709b886aeef
toyo_shun <choka2323@gmail.com>
2021-05-06 12:01:25 +0900

    DB取得

--- src/components/Books/BookList.vue
+++ src/components/Books/BookList.vue
@@ -3,14 +3,16 @@
     <table>
       <thead>
         <tr>
+          <th>ID</th>
           <th>著者</th>
           <th>名前</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="book in books" :key="book.name">
-          <td>{{ book.author }}</td>
+        <tr v-for="(book, index) in books" :key="index">
+          <td>{{ index }}</td>
           <td>{{ book.name }}</td>
+          <td>{{ book.author }}</td>
         </tr>
       </tbody>
     </table>
@@ -18,16 +20,34 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
-import { useStore } from "@/store";
+import { defineComponent, reactive } from "vue";
+import { Book } from "@/store";
+import { db } from "@/scripts/firebase";
 
 export default defineComponent({
   setup() {
-    // Storeを取得する
-    const store = useStore();
+    // // Storeを取得する
+    // const store = useStore();
+
+    // return {
+    //   books: computed(() => store.state.books), // 本の一覧を返す
+    // };
+
+    // リアクティブにしないと初期表示されない
+    const books: Book[] = reactive([]);
+
+    // 全件取得
+    db.collection("books")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id);
+          books.push({name: doc.data().name, author: doc.data().author})
+        });
+      });
 
     return {
-      books: computed(() => store.state.books), // 本の一覧を返す
+      books
     };
   },
 });
